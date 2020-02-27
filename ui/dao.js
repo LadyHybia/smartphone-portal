54be03ae1df15fb15bed539000b9cd760819bbaf
Samah El Banna <samah.elbanna@cgm.com>
2020-02-27 13:45:52 +0100

    deleted unnecessary console.log

--- ui/dao.js
+++ ui/dao.js
@@ -7,7 +7,6 @@ const REQUEST_OPTIONS = {};
 
 /* returns a Promise */
 export function getAll() {
-    console.log('getAll()');
     return fetch(BASE_URL + SMARTPHONE_RESOURCE, REQUEST_OPTIONS)
         .then(response => response.json())
         .then(json => json._embedded[RESOURCE_NAME]);
@@ -15,7 +14,6 @@ export function getAll() {
 
 /* returns a Promise */
 export function findByNameOrManufacturer(searchString) {
-    console.log('findByNameOrManufacturer(' + searchString + ')');
     return fetch(`${BASE_URL}${SMARTPHONE_RESOURCE}${SEARCH}/findByNameContainingIgnoreCaseOrManufacturerContainingIgnoreCase?name=${searchString}&manufacturer=${searchString}&projection=listItem`, REQUEST_OPTIONS)
         .then(response => response.json())
         .then(json => json._embedded[RESOURCE_NAME]);
@@ -27,7 +25,6 @@ export function findById(id) {
 
 /* returns a Promise */
 export function findByNameOrManufacturerAndPriceCategory(searchString, priceCategory) {
-    console.log('findByNameOrManufacturerAndPriceCategory(' + searchString + ', ' + priceCategory + ')');
     return fetch(BASE_URL + SMARTPHONE_RESOURCE + SEARCH + '/findByNameContainingIgnoreCaseAndPriceCategoryOrManufacturerContainingIgnoreCaseAndPriceCategory?name=' + searchString + '&priceCategory1=' + priceCategory + '&manufacturer=' + searchString + '&priceCategory2=' + priceCategory, REQUEST_OPTIONS)
         .then(response => response.json())
         .then(json => json._embedded[RESOURCE_NAME]);
